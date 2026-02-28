1927f7d8d26d7564a74b85da6fded25292449f98
lili-ev <lilinadia101@gmail.com>
2026-02-28 00:28:28 +0000

    java

---
+++ code.js
@@ -0,0 +1,37 @@
+
+console.log('=== TÂCHE 1 ===');
+for (var i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('Counting completed!');
+
+
+console.log('\n=== TÂCHE 2 ===');
+for (var i = 5; i > 0; i--) {
+    console.log(i);
+}
+console.log('Countdown finished!');
+
+console.log('\n=== TÂCHE 3 ===');
+var i = 1;
+while (i < 6) {
+    console.log(i);
+    i++;
+}
+console.log('Counting completed!');
+
+console.log('\n=== TÂCHE 4 ===');
+var i = 5;
+while (i > 0) {
+    console.log(i);
+    i = i - 1; 
+}
+console.log('Countdown finished!');
+
+console.log('\n=== TÂCHE 5 ===');
+var year = 2018;
+while (year < 2023) {
+    console.log(year);
+    year++;
+}
+
